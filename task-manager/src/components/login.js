b59ace2ad3e6753dc9d2a362c17f846d63ed3b3e
Keittah Sewe <kseweo@gmail.com>
2023-03-06 13:20:25 +0300

    login page

---
+++ task-manager/src/components/login.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+
+function Login() {
+  const [fullname, setFullname] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({ fullname, email, password });
+    // add login logic here
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="fullname">Full Name:</label>
+        <input
+          type="text"
+          id="fullname"
+          value={fullname}
+          onChange={(e) => setFullname(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  );
+}
+
+export default Login;
